310f2cb8c57896eaa9cabab3aacb1d9734c7f647
techniponi <wincam97@gmail.com>
2020-01-28 01:35:12 -0600

    reformat

--- zero-server.js
+++ zero-server.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function(req, res) {
-    console.log(req.query);
-    res.send('Hello World!');
+app.get("/", function(req, res) {
+  console.log(req.query);
+  res.send("Hello World!");
 });
 
 app.listen(6969);
