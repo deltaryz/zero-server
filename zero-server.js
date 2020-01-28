46e231ebba0477ac6ba9f5ddca854eb5f4a05a1b
techniponi <wincam97@gmail.com>
2020-01-28 02:26:53 -0600

    move database to project root

--- zero-server.js
+++ zero-server.js
@@ -3,15 +3,16 @@ const sqlite3 = require("sqlite3").verbose();
 const app = express();
 
 // initialize database
-let db = new sqlite3.Database("./db/zero.db", err => {
+let db = new sqlite3.Database("./zero.db", err => {
   if (err) {
     console.error(err.message);
   }
-  console.log("Connected to the zero database.");
+  console.log("Successfully connected to the database.");
 });
 
 // what should we do when we receive a request?
 app.get("/", function(req, res) {
+  console.log("Request received with query:");
   console.log(req.query); // print URL parameters to console
   res.send("Hello World!"); // reply with hello world
 });
