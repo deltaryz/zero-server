e307b21b08b2979e0512ce0ec0812fce79b0e52d
techniponi <wincam97@gmail.com>
2020-01-28 01:44:12 -0600

    add comments

--- zero-server.js
+++ zero-server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+// what should we do when we receive a request?
 app.get("/", function(req, res) {
-  console.log(req.query);
-  res.send("Hello World!");
+  console.log(req.query); // print URL parameters to console
+  res.send("Hello World!"); // reply with hello world
 });
 
+// initiate the server
 app.listen(6969);
